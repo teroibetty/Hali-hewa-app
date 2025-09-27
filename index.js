d773a3d52379e88dc5c5bc95d703d6043c4b3982
Bteroi <teroibetty@gmail.com>
2025-09-27 19:47:34 +0300

    api intergration

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-let apiKey = "7e0d4oc64c0e7t1ff280ab9035a47b70";
-let url =
-  "https://api.shecodes.io/weather/v1/current?query={query}&key={key}&units=metric"
+function showCityWeather(response) {
+  let currentTemp = document.querySelector("#temp-value");
+  let temp = response.data.temperature.current;
+  currentTemp.innerHTML = Math.round(temp);
+  let cityElement = document.querySelector("#input-city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "7e0d4oc64c0e7t1ff280ab9035a47b70";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showCityWeather);
+}
 
 function submitValue(event) {
   event.preventDefault();
   let searchElement = document.querySelector("#search-input-form");
-  let cityElement = document.querySelector("#input-city");
-  cityElement.innerHTML = searchElement.value;
+  searchCity(searchElement.value);
 }
 let searchElement = document.querySelector("#search-form");
 searchElement.addEventListener("submit", submitValue);
+
+searchCity("Sydney");
